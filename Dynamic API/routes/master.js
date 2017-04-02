037da14bd23ef6e968dc8b367cab2c8e7168cf63
JJSeger <jjsbowhunter@yahoo.com>
2017-04-02 19:31:58 -0400

    creating the routes
    creating the routes

--- Dynamic API/routes/master.js
+++ Dynamic API/routes/master.js
@@ -1,3 +1,15 @@
 /**
  * Created by JuddHome on 4/2/17.
  */
+
+
+    /*  This is the import modules  */
+
+    //This defines an express router
+var router = require('express').Router(),
+    //This defines 
+    entrance = require('./paths/entrance'),
+
+    go = require('./paths/go'),
+
+    api = require('./paths/api');
