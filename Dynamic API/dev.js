016417ce08c890cbf05081082aa39ab0530f662c
JJSeger <jjsbowhunter@yahoo.com>
2017-04-02 15:21:06 -0400

    created the .gitignore file as well as a few other files that I may need within the structure-the only content added was within the dev.js file to configure the environmental variables for the database
    created the .gitignore file as well as a few other files that I may
    need within the structure-the only content added was within the dev.js
    file to configure the environmental variables for the database

--- Dynamic API/dev.js
+++ Dynamic API/dev.js
@@ -1,3 +1,21 @@
 /**
  * Created by JuddHome on 4/2/17.
  */
+
+/*This file contains the environmental varibles - You must configure this file with your
+database and environmental variables.
+- Rename this file to development.js to properly connect to the database */
+
+var data={
+
+    sqlLocation:'localhost',
+
+    sqlUsr: 'root',
+
+    sqlPassword: null,
+
+    port: 3000
+
+}
+
+module.exports= data
