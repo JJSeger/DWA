32578a1eaad54b4489eccd1ec6b8698386087e75
JJSeger <jjsbowhunter@yahoo.com>
2017-04-02 19:56:07 -0400

    importing sequelize
    importing sequelize

--- Dynamic API/models/url.js
+++ Dynamic API/models/url.js
@@ -1,5 +1,16 @@
 /**
  * Created by JuddHome on 4/2/17.
+ * This is the URL model
  */
 
 
+
+/*  The code below defines the connection to the database
+and to a table to store and access the urls that are created
+*/
+
+var Sequelize = require('sequelize'),
+
+    cred = require('../development'),
+
+    database = new Sequelize('urlshortener', cred.sqlUser, cred.sqlPassword, { host: cred.sqlLocation }),
