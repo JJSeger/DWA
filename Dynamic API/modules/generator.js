56a193fb8f0a3053d4a845ad0d5b640811a0e0ef
JJSeger <jjsbowhunter@yahoo.com>
2017-04-02 20:23:39 -0400

    export function  for server.js, generating random code caching the generated url then using a loop to make sure there is at least 4 characters but code breaking
    export function  for server.js, generating random code caching the
    generated url then using a loop to make sure there is at least 4
    characters but code breaking

--- Dynamic API/modules/generator.js
+++ Dynamic API/modules/generator.js
@@ -1,3 +1,49 @@
 /**
  * Created by JuddHome on 4/2/17.
  */
+
+
+
+
+var port = require('../development').port;
+
+
+module.exports = ()=>
+{
+    var string = ''
+    values = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ',
+
+        execute = () =
+>
+    {
+        return Math.floor(Math.random() * (values.length + 1));
+    }
+,
+    shortUrl = {};
+
+    function generate() {
+        string = '';
+        for (x = 0; x < 4; x++) {
+
+            string += values.charAt(execute());
+        }
+    }
+
+    generate();
+
+
+    if (string.length < 4 || string.length == 3) {
+
+        generate();
+
+    }
+
+    const gen = {
+        short: 'http://localhost:' + port + '/' + string,
+        code: string
+    }
+
+    return string;
+
+
+}
