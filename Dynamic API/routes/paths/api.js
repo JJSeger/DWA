599cc562cba14bc9997d7193c752827d7f57159a
JJSeger <jjsbowhunter@yahoo.com>
2017-04-02 22:13:12 -0400

    defining the express router
    defining the express router

--- Dynamic API/routes/paths/api.js
+++ Dynamic API/routes/paths/api.js
@@ -1,3 +1,12 @@
 /**
  * Created by JuddHome on 4/2/17.
  */
+
+//this defines the express router
+
+var api = require('express').Router(),
+    //this imports the database connection
+    db = require('../../models/url'),
+    //this imports id and short url gen
+    gen = require('../../modules/generator');
+
