6de7200ec2a16a617b063ea5de2a6e5161c81d44
JJSeger <jjsbowhunter@yahoo.com>
2017-04-02 22:22:08 -0400

    configuring the api route
    configuring the api route

--- Dynamic API/routes/paths/api.js
+++ Dynamic API/routes/paths/api.js
@@ -10,3 +10,46 @@ var api = require('express').Router(),
     //this imports id and short url gen
     gen = require('../../modules/generator');
 
+
+// API routes
+api
+    //returns the urls
+    .get('/', (req, res) => {
+        //this searches the database and returns them as a list
+        db.findAll({}).then((data)=>{ res.json({ data: data }) })
+
+    })
+
+    //this returns one url
+    .get('/:id', (req, res) => {
+        //this is using the id param
+        db.find({ where: { urlId: req.params.id } }).then((data)=>{
+
+            //this returns single url
+            res.json({ data: data })
+
+        })
+
+    })
+
+
+    //creates a url
+    .post('/', (req, res) => {
+
+
+        //data object cache
+        var id = gen(),
+            //instantiate object to be saved and connects data
+            newUrl = {
+            originalUrl: req.body.url,
+            shortUrl: id.short,
+            urlId: id.code
+            };
+
+
+
+
+
+
+
+
