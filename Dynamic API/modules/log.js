630234ec5fb4f12fc98c34bfcfbde05a9cfa956c
JJSeger <jjsbowhunter@yahoo.com>
2017-04-06 17:04:01 -0400

    creating the file that handles the creating of log files while in debug mode
    creating the file that handles the creating of log files while in debug
    mode

--- Dynamic API/modules/log.js
+++ Dynamic API/modules/log.js
@@ -1,3 +1,14 @@
 /**
  * Created by JuddHome on 4/6/17.
  */
+
+
+
+//This file takes care of the logging and creating of log files while in debug mode
+
+
+var fs = require('fs'),
+    path = require('path'),
+    log = {}
+
+log.debug =
