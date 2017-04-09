49c96bbec958fed7f5518053a1949cfc2ed9a202
JJSeger <jjsbowhunter@yahoo.com>
2017-04-09 19:54:55 -0400

    making all the var’s consts
    making all the var’s consts

--- Dynamic API/routes/master.js
+++ Dynamic API/routes/master.js
@@ -6,13 +6,13 @@
     /*  This is to import modules  */
 
     //This defines an express router
-var router = require('express').Router(),
+const router = require('express').Router();
     //This defines the route for the front end
-    entrance = require('./paths/entrance'),
+const entrance = require('./paths/entrance');
     //this is the redirect for the short URL
-    go = require('./paths/go'),
+const go = require('./paths/go');
     //this defines a route for interacting with the database and CRUD
-    api = require('./paths/api');
+const api = require('./paths/api');
 
 
 
--- Dynamic API/routes/paths/go.js
+++ Dynamic API/routes/paths/go.js
@@ -5,25 +5,25 @@
 
 
 // this defines the express router
-var go = require('express').Router(),
+const go = require('express').Router();
 // this imports the database connection
-    db = require('../../models/url')
+const db = require('../../models/url');
 
 // this defines the path for redirect using url id
 go.get('/:id', (req, res) => {
 
     //  this is to search the data base for url with mathching id
-    db.find({where:{urlId:req.params.id}, attributes: ['originalUrl']}).then((data)=>{
+    db.find({where: { urlId:req.params.id }, attributes: ['originalUrl'] }).then((data) => {
 
-    // this is a 200 status and redirect
-    res.status(200).redirect(data.originalUrl)
+        // this is a 200 status and redirect
+        res.status(200).redirect(data.originalUrl);
 
-    // this console.logs route access
-    console.log('###### Redirect route has been accessesed.\n---')
+        // this console.logs route access
+        console.log('###### Redirect route has been accessesed.\n---');
 
-    })
+    });
 
-})
+});
 
 // this exports the module
 module.exports = go;
