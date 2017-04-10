6ee81a820140a8f0abe4498bc957881b378ebab6
JJSeger <jjsbowhunter@yahoo.com>
2017-04-09 20:20:45 -0400

    changing the vars to const
    changing the vars to const

--- Dynamic API/modules/log.js
+++ Dynamic API/modules/log.js
@@ -6,27 +6,32 @@
 
 //This file takes care of the logging and creating of log files while in debug mode
 
+const fs = require('fs');
 
-var fs = require('fs'),
-    path = require('path'),
-    log = {}
+//this caches log object for later exporting
+const log = {};
 
+
+//this configures the logging object
 log.debug = debugState)=> {
 
+    //this checks the process variable
     if(debugState){
 
-        console.log = function(message){
+        console.log = function myLog(message){
 
             fs.appendFile('./logs/logs.md', message, (err)=>{
-                //
+                // this checkes for errors
                 if (err) throw err;
 
-            })
+            });
 
-        }
+        };
 
     }
 
-}
+};
 
-module.exports = log
+
+//this exports the module
+module.exports = log;
