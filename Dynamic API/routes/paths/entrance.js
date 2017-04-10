9d98fb251a7f2759ed67722ef4294f4797ba43ac
JJSeger <jjsbowhunter@yahoo.com>
2017-04-09 22:17:32 -0400

    this console,logs the entrance path
    this console,logs the entrance path

--- Dynamic API/routes/paths/entrance.js
+++ Dynamic API/routes/paths/entrance.js
@@ -4,19 +4,25 @@
 
 
 // this defines the express router
-var entrance = require('express').Router(),
+const entrance = require('express').Router();
 // this it the import path module for absolute paths
-    path = require('path');
+const path = require('path');
 
 // This defines path on the initial app route
 entrance.get('/', (req, res) => {
 
     //sends back response
     res
-    // this defines the response
-    .status(200)
-    // sending the index file
-    .sendFile(path.join(__dirname + '/../../../public/index.html'));
+        // this defines the response
+        .status(200)
+        // sending the index file
+        .sendFile(path.join(__dirname + '/../../../public/index.html'));
+
+        .then((err) => {
+            if (err) throw err;
+            //this console,logs the entrance path
+            console.log('###### Entrance Path has been accessed and successfully executed\n---');
+        });
 
 });
 
