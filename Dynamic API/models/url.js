c8f10142e01c9be77836281e8f8ff9f26c2c06d4
JJSeger <jjsbowhunter@yahoo.com>
2017-04-09 20:25:17 -0400

    changing vars for const and adding semi colons
    changing vars for const and adding semi colons

--- Dynamic API/models/url.js
+++ Dynamic API/models/url.js
@@ -10,57 +10,45 @@
 */
 
 /* Import Modules*/
-console.log('###### Database connection initiated\n---')
+console.log('###### Database connection initiated\n---');
 
 
 
     //Importing Sequelize
-var Sequelize = require('sequelize'),
+const Sequelize = require('sequelize');
     //importing environmental variables
-    cred = require('../development'),
+const cred = require('../development');
     //This defines the database connection string
-    database = new Sequelize('urlshortener', cred.sqlUser, cred.sqlPassword, { host: cred.sqlLocation }),
+const database = new Sequelize('urlshortener', cred.sqlUser, cred.sqlPassword, { host: cred.sqlLocation });
 
     // This defines the model and database
-    Url = database.define('urls', {
+const Url = database.define('urls', {
         //This is for the original URL
-        originalUrl: {
-            type: Sequelize.STRING,
-            required: true
+    originalUrl: {
+        type: Sequelize.STRING,
+        required: true,
 
-        },
+    },
         //This is the redirected url that is generated
-        shortUrl: {
-            type: Sequelize.STRING,
-            required: true
-        },
+    shortUrl: {
+        type: Sequelize.STRING,
+        required: true,
+    },
 
         //The url id is used to search the urls and generate short urls
-        urlId: {
-            type: Sequelize.STRING,
-            required: true
+    urlId: {
+        type: Sequelize.STRING,
+        required: true,
 
-        }
-    })
+    },
+});
 
 
 //syncing the database
-database.sync()
-
+database.sync();
 
 // logging successful connection
-console.log('###### Database connection synced and connection ready\n---')
-
-
+console.log('###### Database connection synced and connection ready\n---');
 
 //expoprting the databse defintion  as a model for interacting
-module.exports = Url
-
-
-
-
-
-
-
-
-
+module.exports = Url;
