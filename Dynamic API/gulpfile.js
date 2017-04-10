7e5587392c936b76e780a0e381d87af6886b4d45
JJSeger <jjsbowhunter@yahoo.com>
2017-04-09 21:13:00 -0400

    // this defines main gulp task  gulp.task('default', function() {     // this assigns task to main gulp task       gulp.watch(['test/**'], ['test']); });  this defines tests task, defines source of test files and doesnt read them and sends files to mocha for running unit tests
    // this defines main gulp task
    gulp.task('default', function() {
        // this assigns task to main gulp task
          gulp.watch(['test/**'], ['test']);
    });
    
    this defines tests task, defines source of test files and doesnt read
    them and sends files to mocha for running unit tests

--- Dynamic API/gulpfile.js
+++ Dynamic API/gulpfile.js
@@ -20,3 +20,21 @@ const mocha = require('gulp-mocha');
 
 //this is the gulp version of nodemon for server restart on changes
 const nodemon = require('gulp-nodemon');
+
+
+// this defines main gulp task
+gulp.task('default', function() {
+    // this assigns task to main gulp task
+    gulp.watch(['test/**'], ['test']);
+});
+
+// this defines tests task
+gulp.task('test', function() {
+
+
+    // this defines source of test files and doesnt read them
+    gulp.src(['test/*.js'], { read: false })
+        // this sends files to mocha for running unit tests
+        .pipe(mocha({ reporter: 'list' }));
+
+});
