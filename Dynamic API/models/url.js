b7f242aec7978e4d1d666fa49efadbf498aea780
JJSeger <jjsbowhunter@yahoo.com>
2017-04-09 22:42:39 -0400

    logging successful connection
    logging successful connection

--- Dynamic API/models/url.js
+++ Dynamic API/models/url.js
@@ -10,37 +10,43 @@
 */
 
     //Importing Sequelize
-var Sequelize = require('sequelize'),
+const Sequelize = require('sequelize');
     //importing environmental variables
-    cred = require('../development'),
+const cred = require('../development');
     //This defines the database connection string
-    database = new Sequelize('urlshortener', cred.sqlUser, cred.sqlPassword, { host: cred.sqlLocation }),
+const database = new Sequelize('urlshortener', cred.sqlUser, cred.sqlPassword, { host: cred.sqlLocation });
 
     // This defines the model and database
-    Url = database.define('urls', {
-        //This is for the original URL
-        originalUrl: {
-            type: Sequelize.STRING,
-            required: true
+const Url = database.define('urls', {
+    //This is for the original URL
+    originalUrl: {
+        type: Sequelize.STRING,
+        required: true
 
-        },
-        //This is the redirected url that is generated
-        shortUrl: {
-            type: Sequelize.STRING,
-            required: true
-        },
+    },
+    //This is the redirected url that is generated
+    shortUrl: {
+        type: Sequelize.STRING,
+        required: true
+    },
 
-        //The url id is used to search the urls and generate short urls
-        urlId: {
-            type: Sequelize.STRING,
-            required: true
+    //The url id is used to search the urls and generate short urls
+    urlId: {
+        type: Sequelize.STRING,
+        required: true
 
-        }
-    })
+    }
+});
 
 
 //syncing the database
 database.sync()
+
+
+//logging successful connection
+console.log('###### Database connection synced and connection ready\n---');
+
+
 
 //expoprting the databse defintion  as a model for interacting
 module.exports = Url
