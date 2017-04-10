baad8b0fab2ad93272462259c06bfdc0ff74095e
JJSeger <jjsbowhunter@yahoo.com>
2017-04-09 21:52:42 -0400

    changed the var to const and  connected to the master path
    changed the var to const and  connected to the master path

--- Dynamic API/routes/master.js
+++ Dynamic API/routes/master.js
@@ -6,13 +6,13 @@
     /*  This is to import modules  */
 
     //This defines an express router
-var router = require('express').Router(),
+const router = require('express').Router();
     //This defines the route for the front end
-    entrance = require('./paths/entrance'),
+const entrance = require('./paths/entrance');
     //this is the redirect for the short URL
-    go = require('./paths/go'),
+const go = require('./paths/go');
     //this defines a route for interacting with the database and CRUD
-    api = require('./paths/api');
+const api = require('./paths/api');
 
 
 
@@ -23,7 +23,13 @@ router
     //this connects the front page
     .use('/', entrance)
     //this connects the redirect route
-    .use('/go', go)
+    .use('/go', go);
+
+
+
+// this connects to the master path
+console.log('###### Master Path Loaded\n---');
+
 
 //This exports router for use in server
 module.exports = router;
