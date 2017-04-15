c49ccae7d1ea48ac58792105d4c7213b5dbf02c6
JJSeger <jjsbowhunter@yahoo.com>
2017-04-15 14:07:04 -0400

    This is require URL
    This is require URL

--- Dynamic API/routes/api/url.js
+++ Dynamic API/routes/api/url.js
@@ -1,3 +1,10 @@
 /**
  * Created by JuddHome on 4/15/17.
  */
+// This is require URL
+var url = require('../../models/url');
+var shortUrl = require('../../modules/main_url');
+
+module.exports = (express) => {
+    // This is the express router
+    var router = express.Router();
