16d221b1f101e395ff68622c95e3028f41c124ec
JJSeger <jjsbowhunter@yahoo.com>
2017-04-15 15:07:36 -0400

    adding my constants
    adding my constants

--- Dynamic API/test/__url.js
+++ Dynamic API/test/__url.js
@@ -1,3 +1,22 @@
 /**
  * Created by JuddHome on 4/15/17.
  */
+
+
+const express = require('express');
+const expect = require('chai').expect;
+const request = require('supertest');
+const shortUrl = require('../src/modules/main_url');
+const app = express();
+
+
+describe('URL Routes', () => {
+    let server;
+
+beforeEach(() => {
+    server = require('../src/server.js');
+});
+
+afterEach(() => {
+    server.close();
+});
