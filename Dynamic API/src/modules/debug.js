1b44752609296e0efc0ffe15998ce775cc0495dc
JJSeger <jjsbowhunter@yahoo.com>
2017-04-15 14:36:10 -0400

    adding if statements
    adding if statements

--- Dynamic API/src/modules/debug.js
+++ Dynamic API/src/modules/debug.js
@@ -11,3 +11,25 @@ exports.debug = (data, status, date)=>{
     const red = '\x1B[31m';
     const green = '\x1b[32m';
     const blue = '\x1b[34m';
+
+    if(status !== "Congrats!! Success!") {
+
+        const data = blue + time + red + status + data;
+
+    }else{
+
+        data = blue + time + data + ': ' + green + status;
+    }
+
+    if(process.env.DEBUG === "true"){
+        // This creates the dreaded but useful log file
+        fs.appendFile('./logs/debug.log', data, (err) =>{
+            if(err){
+            return console.log(err);
+        }
+
+    });
+    console.log(data);
+}
+
+};
