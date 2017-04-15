fe4b16bbe62ed3cb0e5c8d8b3abfc16b8909d42a
JJSeger <jjsbowhunter@yahoo.com>
2017-04-15 14:07:36 -0400

    This creates
    This creates

--- Dynamic API/routes/api/url.js
+++ Dynamic API/routes/api/url.js
@@ -8,3 +8,13 @@ var shortUrl = require('../../modules/main_url');
 module.exports = (express) => {
     // This is the express router
     var router = express.Router();
+
+    // This creates
+    router.post('/url', (req, res) => {
+        req.body.shortened_url = shortUrl.shortUrl();
+    url.create(req.body, (err) => {
+        res.status(500).json(err);
+}, (data) => {
+    res.status(200).json(data);
+});
+});
