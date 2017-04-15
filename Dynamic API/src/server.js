7b99094943d347e695c3bf7ffabb0ddc8d69cd4c
JJSeger <jjsbowhunter@yahoo.com>
2017-04-15 14:59:53 -0400

    These are dependencies
    These are dependencies

--- Dynamic API/src/server.js
+++ Dynamic API/src/server.js
@@ -1,3 +1,7 @@
 /**
  * Created by JuddHome on 4/15/17.
  */
+// These are dependencies
+const express = require('express');
+const bodyParser = require('body-parser');
+const debug = require('nx-debugtool');
