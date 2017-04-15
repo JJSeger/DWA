4ec2831d07f281d0e4bfc848c27cbafbf9cfe4e2
JJSeger <jjsbowhunter@yahoo.com>
2017-04-15 14:34:56 -0400

    these are color constiables
    these are color constiables

--- Dynamic API/src/modules/debug.js
+++ Dynamic API/src/modules/debug.js
@@ -6,3 +6,8 @@ const fs = require('fs');
 exports.debug = (data, status, date)=>{
     // This is the constiable for timestamp
     const time = new Date() + '\n';
+
+    // these are colour constiables (RGB)
+    const red = '\x1B[31m';
+    const green = '\x1b[32m';
+    const blue = '\x1b[34m';
