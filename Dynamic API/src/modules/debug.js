b5481da9b6fb67fdc60b46b678f86c0f47a24e49
JJSeger <jjsbowhunter@yahoo.com>
2017-04-15 14:34:18 -0400

    This is the constiable for timestamp
    This is the constiable for timestamp

--- Dynamic API/src/modules/debug.js
+++ Dynamic API/src/modules/debug.js
@@ -1,3 +1,8 @@
 /**
  * Created by JuddHome on 4/15/17.
  */
+const fs = require('fs');
+
+exports.debug = (data, status, date)=>{
+    // This is the constiable for timestamp
+    const time = new Date() + '\n';
