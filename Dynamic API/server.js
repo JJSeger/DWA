0b5e41fdcfa436975617b8774709e1149823d323
JJSeger <jjsbowhunter@yahoo.com>
2017-04-15 13:45:49 -0400

    setting up routes, configuration for the server and server export
    setting up routes, configuration for the server and server export

--- Dynamic API/server.js
+++ Dynamic API/server.js
@@ -4,3 +4,19 @@ var express = require('express');
 var bodyParser = require('body-parser');
 // This is express
 var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true,
+}));
+
+// This is routes
+app.use('/', require('./routes/global.js')(express));
+
+// This is the configuration for the server
+var port = process.env.PORT || 3000;
+
+// This is the export server
+exports.server = app.listen(port, () => {
+        console.log('Server Active On', port);
+});
