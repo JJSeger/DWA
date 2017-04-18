06107c0a43af50780e19296a6fa73053ca35443c
JJSeger <jjsbowhunter@yahoo.com>
2017-04-17 20:46:39 -0400

    added Arguments
     added Arguments

--- Dynamic API/version.js
+++ Dynamic API/version.js
@@ -7,3 +7,8 @@
 
 const pjson = require('./package.json');
 const fs = require('fs');
+
+//  added Arguments
+const argv = require('yargs').argv;
+const version = pjson.version.split('.');
+const number = arg.n;
