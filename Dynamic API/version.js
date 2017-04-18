dd9f24207ada10d98cfe71cb82410ef91e4b842e
JJSeger <jjsbowhunter@yahoo.com>
2017-04-17 20:47:13 -0400

    This evaluates arguments to see if they pass and are correct
    This evaluates arguments to see if they pass and are correct

--- Dynamic API/version.js
+++ Dynamic API/version.js
@@ -12,3 +12,15 @@ const fs = require('fs');
 const argv = require('yargs').argv;
 const version = pjson.version.split('.');
 const number = arg.n;
+
+
+// This evaluates arguments to see if they pass and are correct
+if ((argv.v === 'major' || argv.v === 'minor' || argv.v === 'patch') && typeof argv.n === 'number')
+{
+    if (argv.v === 'major') {
+        version[0] = number;
+    } else if (argv.v === 'minor') {
+        version[1] = number;
+    } else if (argv.v === 'patch') {
+        version[2] = number;
+    }
