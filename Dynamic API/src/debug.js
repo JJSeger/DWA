bc979e56f7271f8432fde770c42d0615b71860a4
JJSeger <jjsbowhunter@yahoo.com>
2017-04-17 20:28:08 -0400

    created debug,js
    created debug,js

--- Dynamic API/src/debug.js
+++ Dynamic API/src/debug.js
@@ -35,3 +35,31 @@ exports.verBump = (currVer, bump) => {
     // Minor index is verArr[1]
     // Patch index is verArr[2]
     const verArr = currVer.split('.');
+
+
+
+    for (const i in verArr) {
+        verArr[i] = parseInt(verArr[i], 10);
+    }
+
+    if (bump === 'patch') {
+        // patch
+        verArr[2] += 1;
+        console.log('A Patch update has been created, version: ', verArr[0], '.', verArr[1], '.', verArr[2]);
+    } else if (bump === 'minor') {
+        // patch
+        verArr[2] = 0;
+        // minor
+        verArr[1] += 1;
+        console.log('A Minor update has been created, version: ', verArr[0], '.', verArr[1], '.', verArr[2]);
+    } else if (bump === 'major') {
+        // patch
+        verArr[2] = 0;
+        // minor
+        verArr[1] = 0;
+        verArr[0] += 1;
+        console.log('A Major update has been created, version: ', verArr[0], '.', verArr[1], '.', verArr[2]);
+    }
+
+    return verArr.join('.');
+};
