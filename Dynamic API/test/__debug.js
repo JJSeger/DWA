f0f4585bd5d8db38b144e1e8f9d2beeb0b3edb74
JJSeger <jjsbowhunter@yahoo.com>
2017-04-17 20:35:22 -0400

    created__debug.js and adde global dependencies
    created__debug.js and adde global dependencies

---
+++ Dynamic API/test/__debug.js
@@ -0,0 +1,9 @@
+/**
+ * Created by JuddHome on 4/17/17.
+ */
+
+
+// These are the global dependencies
+const expect = require('chai').expect;
+const util = require('../src/debug');
+require('mocha-sinon');
