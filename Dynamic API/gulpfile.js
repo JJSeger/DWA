0452d10d49ddf3e34628431ba2e99331921378bb
JJSeger <jjsbowhunter@yahoo.com>
2017-04-17 20:45:24 -0400

    deleted server.js and mcreated gulp file.js and version.js
    deleted server.js and mcreated gulp file.js and version.js

---
+++ Dynamic API/gulpfile.js
@@ -0,0 +1,24 @@
+/**
+ * Created by JuddHome on 4/17/17.
+ */
+
+
+const gulp = require('gulp');
+const bump = require('gulp-bump');
+const argv = require('yargs').argv;
+
+const version = '';
+
+if (argv.major) {
+    version = 'major';
+} else if (argv.minor) {
+    version = 'minor';
+} else {
+    version = 'patch';
+}
+
+gulp.task('bump', () => {
+    gulp.src('./package.json')
+    .pipe(bump({type:version}))
+    .pipe(gulp.dest('./'));
+});
---
+++ Dynamic API/version.js
@@ -0,0 +1,5 @@
+/**
+ * Created by JuddHome on 4/17/17.
+ */
+
+
